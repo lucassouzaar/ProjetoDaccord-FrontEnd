f1ec6fa1e81b41bea92067af86a52dbb56b2cb2a
mposnik <murilloposnik@hotmail.com>
2023-05-25 21:15:51 -0300

    teste

---
+++ _javascript/home.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var swiper = new Swiper(".swiper-container", {
+    loop: true,
+    autoplay: {
+      delay: 4000, // Tempo em milissegundos (4 segundos)
+    },
+  });
+});
